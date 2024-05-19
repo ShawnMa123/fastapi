5896df16aac60ab7fa2c10aebc7f6ccc998f4be5
Shawn Ma <1997mwb@gmail.com>
2024-05-19 17:13:11 +0800

    update more details

--- Project 1/books.py
+++ Project 1/books.py
@@ -1,7 +1,6 @@
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
-
 
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
@@ -34,7 +33,6 @@ async def read_category_by_query(category: str):
     return books_to_return
 
 
-# Get all books from a specific author using path or query parameters
 @app.get("/books/byauthor/")
 async def read_books_by_author_path(author: str):
     books_to_return = []
@@ -43,7 +41,6 @@ async def read_books_by_author_path(author: str):
             books_to_return.append(book)
 
     return books_to_return
-
 
 @app.get("/books/{book_author}/")
 async def read_author_category_by_query(book_author: str, category: str):
@@ -55,22 +52,6 @@ async def read_author_category_by_query(book_author: str, category: str):
 
     return books_to_return
 
-
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
-
-
-@app.put("/books/update_book")
-async def update_book(updated_book=Body()):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
-            BOOKS[i] = updated_book
-
-
-@app.delete("/books/delete_book/{book_title}")
-async def delete_book(book_title: str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
-            BOOKS.pop(i)
-            break
